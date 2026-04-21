305afb8e89b98fadecab0c4acca20f83aaedc6bb
PabloGonzalezGuerrero <pablo.gonzalezguerre@edu.uah.es>
2026-04-21 18:09:39 +0200

    adas

--- Prac5/UAH_ASW_Prac4_Done/UAH_ASW_Prac4_Done/ClasesActor/CCHK_FDIRMngC.cpp
+++ Prac5/UAH_ASW_Prac4_Done/UAH_ASW_Prac4_Done/ClasesActor/CCHK_FDIRMngC.cpp
@@ -65,10 +65,15 @@ void	CCHK_FDIRMng::EDROOM_CTX_Top_0::FDoHK_FDIR()
 {
    //Define absolute time
   Pr_Time time;
+	 
+	//Timing Service useful methods
+	 
+	//time.GetTime(); // Get current monotonic time
+	//time.Add(X,Y); // Add X sec + Y microsec
  
 VNextTimeout+= Pr_Time(1,0); // Add X sec + Y microsec 
 time=VNextTimeout; 
- 
+pus_service4_update_all_stats();
 pus_services_update_params();
 pus_service3_do_HK();
    //Program absolute timer 
